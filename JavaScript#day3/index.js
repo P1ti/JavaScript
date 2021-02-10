808862b9b68925625e04b6dd71a37814e74018bc
P1ti <petricaberbecea@yahoo.com>
2021-02-10 15:28:14 +0200

    JavaScript#day3

---
+++ JavaScript#day3/index.js
@@ -0,0 +1,36 @@
+      //While and for loops
+
+      //While syntax
+      while (true) {
+
+      }
+
+      //The code inside while loop is executed if the condition is truthy
+      let i = 0;
+      while (i < 3) {
+        console.log(i);
+        i++;
+      }
+
+
+      //Do...while syntax
+      do {
+
+      } while (true);
+
+      //This loop will first execute the body, then check the condition, while is truthy execute it again and again
+
+      let i = 0;
+      do {
+        console.log(i);
+        i++;
+      } while (i < 3);
+
+      //For syntax
+      for (begin, condition, step) {
+
+      }
+
+      for (var i = 0; i < 3; i++) {
+        console.log(i);
+      }
