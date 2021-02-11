22a02f6180ed0b9a702ca39631de2f35b9ae9d8d
P1ti <petricaberbecea@yahoo.com>
2021-02-11 14:26:10 +0200

    JavaScript#day4

--- JavaScript#day4/index.js
+++ JavaScript#day4/index.js
@@ -1,7 +1,32 @@
  //Exercises
- let numberOfPassengers = 10;
+
+  // for loop
+  let numberOfPassengers = 10;
 
   for (let i = 0; i < numberOfPassengers; i++) {
     console.log("Passenger boarded");
   }
   console.log("All passengers boarded");
+
+  //while loop
+  let numberOfPassengers = 10;
+  let i = 0;
+
+  while (i < numberOfPassengers) {
+    console.log("Passenger boarded");
+    i++;
+  }
+  console.log("All passengers boarded");
+
+  //for looping using an array
+  const passengers = [
+    "Karter Hudson",
+    "Aahil Faulkner",
+    "Coby Mcarthur",
+    "Grant Campbell",
+    "Herbert Whyte"
+  ];
+
+  for (let i in passengers) {
+    console.log("Passenger: " + passengers[i] + " boarded");
+  }
